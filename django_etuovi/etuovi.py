59d67b4d472dd40dd6d460bfbca42d49617d8410
Kristian Klemets <kristian.klemets@anders.fi>
2020-09-21 10:36:35 +0300

    Refactor the library helper functions
    - Add typing.
    - Updated etree building to look nicer and to match the example
    xml format.
      - Item type will always be "update".
    - Update xml sending.
      - Updated filename.
      - File should have a temp name while sending.
    - All the settings variables should be gained from Etuovi.
    
    Refs ASU-204 | Django: Etuovi API: Update apartment creation

--- django_etuovi/etuovi.py
+++ django_etuovi/etuovi.py
@@ -1,36 +1,44 @@
 import time
 from ftplib import FTP
+from typing import List
 from xml.etree.ElementTree import ElementTree
 
 from django.conf import settings
 from lxml import etree
 
+from django_etuovi.items import BaseClass
 
-def get_filename():
-    # TODO change this when actual filename confirmed.
-    return "EO_realty2_{}.xml".format(
+
+def create_element_tree(items: List[BaseClass]) -> etree.Element:
+    root = etree.Element("transferData", version="1.0")
+    group = etree.Element("transferGroup", type="all", name=settings.ETUOVI_TRANSFER_ID)
+    root.append(group)
+    for item in items:
+        group.append(item.to_etree())
+    return root
+
+
+def get_filename() -> str:
+    return "{}_all_{}.xml".format(
+        settings.ETUOVI_COMPANY_NAME,
         time.strftime("%Y%m%d%H%M%S"),
     )
 
 
-def send_items(items, transfer_type="all"):
-    root = etree.Element("transferData", version="1.0")
-    group = etree.Element(
-        "transferGroup", type=transfer_type, name=settings.ETUOVI_TRANSFER_ID
-    )
-    root.append(group)
-    for item in items:
-        group.append(item.to_etree())
-    tree = ElementTree(group)
-    filename = get_filename()
-    tree.write(filename, encoding="utf-8", xml_declaration=True)
-
-    session = FTP(
+def get_session() -> FTP:
+    return FTP(
         host=settings.ETUOVI_FTP_HOST,
         user=settings.ETUOVI_USER,
         passwd=settings.ETUOVI_PASSWORD,
     )
-    # TODO Confirm if the temp filename is required and if quit is required.
+
+
+def send_items(items: List[BaseClass]) -> None:
+    element_tree = ElementTree(create_element_tree(items))
+    filename = get_filename()
+    element_tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+    session = get_session()
     session.storbinary("{}.temp".format(filename), open(filename, "rb"))
     session.rename("{}.temp".format(filename), filename)
     session.quit()
